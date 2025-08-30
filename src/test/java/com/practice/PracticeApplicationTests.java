b3a57abe8092466ebfb5adb2c551834a9fe08e02
Minju Kim <mminju.km@gmail.com>
2025-08-31 00:57:31 +0900

    Fix: 테스트 yml 수정

--- src/test/java/com/practice/PracticeApplicationTests.java
+++ src/test/java/com/practice/PracticeApplicationTests.java
@@ -1,10 +1,8 @@
 package com.practice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
 @ActiveProfiles("test")
 class PracticeApplicationTests {
 
